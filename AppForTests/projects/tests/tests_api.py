b121ea6613645c416339d6a8e98a6fede9438bb3
Andrew <andrey-zheleznyak124@yandex.ru>
2024-06-16 23:06:18 +0300

    Adding two post tests

--- AppForTests/projects/tests/tests_api.py
+++ AppForTests/projects/tests/tests_api.py
@@ -10,16 +10,24 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppForTests.settings')
 django.setup()
 
 
-@allure.tag("api", "positive")
+def http_request(method, url=reverse("crypto_projects-list"), data=None, content_type='application/json'):
+    client = Client()
+    request_method = getattr(client, method.lower())
+    if data:
+        response = request_method(url, data=json.dumps(data), content_type=content_type)
+    else:
+        response = request_method(url)
+    return response
+
+
+@allure.tag("api", "get")
 class TestsGetCryptoProjects(TestCase):
     @allure.title("Verify non-empty list of crypto projects")
     @allure.description("Send a GET request to the API and verify that the response contains a non-empty list of "
                         "crypto projects")
     def test_get_list_content_with_crypto_projects(self) -> None:
-        client = Client()
-        crypto_projects_url = reverse("crypto_projects-list")
         with allure.step("Send GET request to retrieve the list of projects"):
-            response = client.get(crypto_projects_url)
+            response = http_request("GET")
         with allure.step("Verify that the response status code is 200"):
             self.assertEqual(response.status_code, 200), f"Error. Response status code is {response.status_code}"
         with allure.step("Check that the list of projects is not empty"):
@@ -52,3 +60,28 @@ class TestsGetCryptoProjects(TestCase):
                 with allure.step(f"Verify project name {expected_name} for project ID {project_id}"):
                     self.assertEqual(json_response["name"], expected_name)
 
+    def test_one_project_exists_should_succeed(self):
+        response = http_request("GET")
+        response_content = json.loads(response.content)[0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_content.get("id"), 1)
+        self.assertEqual(response_content.get("name"), "Bitcoin")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+
+@allure.tag("api", "post")
+class TestsPostCryptoProjects(TestCase):
+    def test_adding_already_created_crypto_project(self):
+        data = {
+            "id": 7,
+            "name": "Sui"}
+        response = http_request("POST", data=data)
+        self.assertTrue(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), {"name": ["crypto projects with this name already exists."]})
+
+    def test_adding_new_crypto_project_without_required_field(self):
+        data = {"id": 8}
+        response = http_request("POST", data=data)
+        self.assertTrue(response.status_code, 400)
+        self.assertEqual(json.loads(response.content), {"name": ["This field is required."]})
